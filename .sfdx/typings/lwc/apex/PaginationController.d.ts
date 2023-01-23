83bf3538997e6760b776e1959a417c3975c3c636
akshayac4 <akshaychauhan.ac4@gmail.com>
2023-01-23 11:36:44 +0530

    Add

---
+++ .sfdx/typings/lwc/apex/PaginationController.d.ts
@@ -0,0 +1,9 @@
+declare module "@salesforce/apex/PaginationController.getDefault" {
+  export default function getDefault(): Promise<any>;
+}
+declare module "@salesforce/apex/PaginationController.saveContact" {
+  export default function saveContact(param: {listt: any}): Promise<any>;
+}
+declare module "@salesforce/apex/PaginationController.searchContact" {
+  export default function searchContact(param: {firstName: any}): Promise<any>;
+}
